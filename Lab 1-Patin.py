803b380266a09f2c44aed549650b46b5b16e9f55
spatin01 <157068598+spatin01@users.noreply.github.com>
2024-01-25 16:01:44 -0500

    Add files via upload

---
+++ Lab 1-Patin.py
@@ -0,0 +1,73 @@
+Python 3.12.1 (v3.12.1:2305ca5144, Dec  7 2023, 17:23:38) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+#Names: Samantha Patin and Elise Ward
+print("Hello, world")
+Hello, world
+print("Welcome to CPSC 207")
+Welcome to CPSC 207
+print("Hello\nWorld")
+Hello
+World
+>>> print("How \nare \nyou?")
+How 
+are 
+you?
+>>> print("Today" + "is" + "Wednesday")
+TodayisWednesday
+>>> print("a\nb\nc")
+a
+b
+c
+>>> print("Hello" +"World")
+HelloWorld
+>>> print("Hello\n" + "\nWorld"!)
+SyntaxError: invalid syntax
+>>> print("Hello\n" + "\nWorld!")
+Hello
+
+World!
+>>> print("123/n45\n\6")
+123/n45
+
+>>> print("123\n45\n\6")
+123
+45
+
+>>> print("123\n45\n6")
+123
+45
+6
+>>> 1+2*3
+7
+>>> (1+2)*3
+9
+>>> 7/3
+2.3333333333333335
+>>> 7//3
+2
+>>> 7%3
+1
+>>> 22/4
+5.5
+>>> 22//4
+5
+>>> 22%4
+2
+>>> 3**2
+9
+>>> 2**3
+8
+>>> #Prediction: 7
+>>> (4+3)//2
+3
+>>> #Prediction: 0
+>>> 6%2
+0
+>>> 1+2*3**4
+163
+>>> print("2+2=", 2+2)
+2+2= 4
+>>> print("2+2=",2+2)
+2+2= 4
+>>> print("50 pounds is", 50*0.453592, "Kilograms")
+50 pounds is 22.6796 Kilograms
